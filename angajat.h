2721d55b4b6b7f73145151cd5104c141bcc27ec9
HoriaTG <enescu.horia@yahoo.com>
2023-01-30 15:52:19 +0200

    Bifa

--- angajat.h
+++ angajat.h
@@ -6,13 +6,13 @@
 
 class angajat
 {
-    static std::string nume_firma; // firma la care e angajat personalul magazinului
 private:
+    static std::string nume_firma; // firma la care e angajat personalul magazinului
     std::string nume;
     double salariu;
     int ani_experienta;
 public:
-    explicit angajat(const std::string &nume_firma,const std::string &nume,double salariu,int ani_experienta);
+    angajat(const std::string &nume_firma,const std::string &nume,double salariu,int ani_experienta);
     friend std::ostream &operator<<(std::ostream &os, const angajat &ang);
     void promovare();
 };
